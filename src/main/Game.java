619913541b28a2b3f1430be9617630e38289b5f8
Jasper Delight <jasperdelightt@gmail.com>
2023-09-23 00:31:51 -0600

    window and red square

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,10 +1,25 @@
 package main;
 
-public class Game {
+import javax.swing.JFrame;
+
+public class Game extends JFrame{
+
+	private GameScreen gameScreen;
+
+	public Game(){
+
+		setSize(400, 400);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		gameScreen = new GameScreen();
+		add(gameScreen);
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("hello world");
+		Game game = new Game();
 
 	}
 
