395dd910c5b3ee6e201ea5d9d4a414a5230303b9
Jasper Delight <jasperdelightt@gmail.com>
2023-09-23 01:11:41 -0600

    img added

--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -1,16 +1,38 @@
 package main;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
 import javax.swing.JPanel;
 
 public class GameScreen extends JPanel{
 
-	public GameScreen() {
-		
+	private Random random;
+	private BufferedImage img;
+
+	public GameScreen(BufferedImage img) {
+		this.img = img;
+		random = new Random();
 	}
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.RED);
-		g.fillRect(50, 50, 100, 100);
+		// g.setColor(Color.RED);
+		// g.fillRect(0, 0, 32, 32);
+		// g.fillRect(32, 0, 32, 32);
+		g.drawImage(img.getSubimage(32*9, 32, 32, 32), 0, 0, null);
+
+		// for(int y = 0; y < 20; y++){
+		// 	for(int x = 0; x < 20; x++){
+		// 		g.setColor(getRndColor());
+		// 		g.fillRect(x * 32, y * 32 ,32, 32);
+		// 	}
+		// }
+	}
+	private Color getRndColor(){
+		int r = random.nextInt(256);
+		int g = random.nextInt(256);
+		int b = random.nextInt(256);
+		return new Color(r, g, b);
 	}
 }
