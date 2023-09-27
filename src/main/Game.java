ef4c49e0e9250c367b8aa4ee0c34607875295cb5
Jasper Delight <jasperdelightt@gmail.com>
2023-09-26 19:00:43 -0600

    added thread

--- src/main/Game.java
+++ src/main/Game.java
@@ -7,14 +7,13 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-public class Game extends JFrame{
+public class Game extends JFrame implements Runnable{
 
 	private GameScreen gameScreen;
 	private BufferedImage img;
 
 
-	private double timePerFrame;
-	private long lastFrame;
+
 
 	private long lastUpdate;
 	private double timePerUpdate;
@@ -22,9 +21,11 @@ public class Game extends JFrame{
 	private int updates;
 	private long lastTimeUPS;
 
+	private Thread gameThread;
+
 	public Game(){
 		
-		timePerFrame = 1000000000.0 / 120.0;
+
 		timePerUpdate = 1000000000.0 / 60.0;
 		
 
@@ -44,22 +45,15 @@ public class Game extends JFrame{
 			e.printStackTrace();
 		}
 	}
+	private void start(){
+		gameThread = new Thread(this){};
+		gameThread.start();
+	}
 	private void loopGame(){
 		while(true){
 
-			if(System.nanoTime()- lastUpdate >= timePerUpdate){
-				updateGame();
-				callUPS();
-			}
 
-			if(System.nanoTime() - lastFrame >= timePerFrame){
-				lastFrame = System.nanoTime();
-				repaint();
-			}else{
-				
-			}
-		}
-	}
+	}}
 	private void callUPS(){
 		if(System.currentTimeMillis() - lastTimeUPS >= 1000){
 			System.out.println("UPS: "  + updates);
@@ -76,7 +70,23 @@ public class Game extends JFrame{
 	public static void main(String[] args) {
 		
 		Game game = new Game();
-		game.loopGame();
+		game.start();
+	}
+	@Override
+	public void run() {
+		double timePerFrame;
+		long lastFrame = System.nanoTime();
+				timePerFrame = 1000000000.0 / 120.0;
+		while(true){
+			if(System.nanoTime() - lastFrame >= timePerFrame){
+				lastFrame = System.nanoTime();
+				repaint();
+			}
+			if(System.nanoTime()- lastUpdate >= timePerUpdate){
+				updateGame();
+				callUPS();
+			}
+		}
 	}
 
 }
